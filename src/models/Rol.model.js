fa30ecfc46d59e98fbdd3d4535efa8042e5b9cdb
Velsty <117530498+Velsty@users.noreply.github.com>
2025-07-09 14:09:21 -0500

    asd

--- src/models/Rol.model.js
+++ src/models/Rol.model.js
@@ -30,6 +30,7 @@ module.exports = (sequelize, DataTypes) => {
       tipoPerfil: {
         type: DataTypes.STRING,
         allowNull: false,
+        field: "tipo_perfil",
         validate: {
           isIn: [["CLIENTE", "EMPLEADO"]],
         },
@@ -50,7 +51,7 @@ module.exports = (sequelize, DataTypes) => {
 
     // Un rol puede tener muchos permisos a través de la tabla de unión.
     Rol.belongsToMany(models.Permisos, {
-      through: "permisos_x_rol",
+      through: "permisos_x_rol", 
       foreignKey: "id_rol", // Clave foránea en la tabla de unión.
       otherKey: "id_permiso", // La otra clave foránea en la tabla de unión.
       as: "permisos",
