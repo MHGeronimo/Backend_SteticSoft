7e120fba4abe58992c46b4f28a0468b3c0ebb6f6
MHGeronimo <mrgerito@gmail.com>
2025-09-05 15:36:40 -0500

    agregar id_categproa

--- steticsoft_schema.sql
+++ steticsoft_schema.sql
@@ -306,7 +306,8 @@ CREATE TABLE IF NOT EXISTS abastecimiento (
     razon_agotamiento TEXT,
     fecha_agotamiento DATE,
     estado BOOLEAN DEFAULT TRUE NOT NULL,
-    id_usuario INT REFERENCES usuario(id_usuario) ON DELETE SET NULL
+    id_usuario INT REFERENCES usuario(id_usuario) ON DELETE SET NULL,
+    id_categoria_producto INT REFERENCES categoria_producto(id_categoria_producto) ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS token_recuperacion (
